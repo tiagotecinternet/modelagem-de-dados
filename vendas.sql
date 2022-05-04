1f36d4adcbe0d218b248b03a2d8c45728945985c
tiagotecinternet <tiago.tecinternet22@gmail.com>
2022-05-04 11:59:22 -0300

    Update vendas.sql

--- vendas.sql
+++ vendas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 27-Abr-2022 às 16:54
+-- Tempo de geração: 04-Maio-2022 às 16:58
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.0.13
 
@@ -32,6 +32,19 @@ CREATE TABLE `fabricantes` (
   `nome` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Extraindo dados da tabela `fabricantes`
+--
+
+INSERT INTO `fabricantes` (`id`, `nome`) VALUES
+(1, 'Asus'),
+(2, 'Dell'),
+(3, 'Apple'),
+(5, 'Samsung'),
+(6, 'Brastemp'),
+(7, 'Positivo'),
+(8, 'Microsoft Brasil');
+
 -- --------------------------------------------------------
 
 --
@@ -43,8 +56,21 @@ CREATE TABLE `produtos` (
   `nome` varchar(45) NOT NULL,
   `descricao` text NOT NULL,
   `preco` decimal(6,2) NOT NULL,
+  `quantidade` tinyint(4) NOT NULL,
   `fabricante_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `produtos`
+--
+
+INSERT INTO `produtos` (`id`, `nome`, `descricao`, `preco`, `quantidade`, `fabricante_id`) VALUES
+(1, 'Ultrabook', 'Ultrabook Asus do Brasil com processador Intel Core i12, memória RAM de 16GB e Windows 11', '6500.99', 15, 1),
+(2, 'Tablet Android', 'Tablet com a versão 12 do sistema operacional da Google, possui tela de 10 polegadas e armazenamento de 64GB.', '4999.00', 3, 5),
+(4, 'iPhone 13 Pro Max', 'Alta durabilidade, processador Bionic 14, 128 GB de armazenamento, 6GB de RAM e caro pra caramba', '6999.99', 15, 3),
+(5, 'iPad Mini 2', 'Tablet Apple com tela retina display de 4k, memória interna de 64GB, acesso ao iCloud.', '5000.00', 15, 3),
+(6, 'Xbox', 'Console de última geração com acesso aos melhores jogos e bla bla', '2500.00', 6, 8),
+(7, 'Ultrabook', 'Equipamento com processador AMD Ryzen5, 12GB de RAM, placa de vídeo RTX', '4500.68', 12, 7);
 
 --
 -- Índices para tabelas despejadas
@@ -71,13 +97,13 @@ ALTER TABLE `produtos`
 -- AUTO_INCREMENT de tabela `fabricantes`
 --
 ALTER TABLE `fabricantes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de tabela `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Restrições para despejos de tabelas
